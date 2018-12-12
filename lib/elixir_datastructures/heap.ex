0873e73769462a4fbaab49b36a482c1537b728a8
John Goff <john.g@hiredhippo.ai>
2018-12-12 12:45:33 -0500

    add documentation

--- lib/elixir_datastructures/heap.ex
+++ lib/elixir_datastructures/heap.ex
@@ -12,12 +12,10 @@ defmodule LeftistHeap do
   Will return 0 for a leaf.
 
   ## Examples
-
-      iex> Heap.rank(%Heap{rank: 2})
-      2
-
-      iex> Heap.rank(nil)
-      0
+  iex> Heap.rank(%Heap{rank: 2})
+  2
+  iex> Heap.rank(nil)
+  0
   """
   def rank(%LeftistHeap{rank: rank}), do: rank
   def rank(nil), do: 0
@@ -31,6 +29,13 @@ defmodule LeftistHeap do
   """
   def from_key(key) when is_integer(key), do: %LeftistHeap{key: key, rank: 1}
 
+  @doc """
+  Merges two heaps together
+
+  ## Examples
+  iex> LeftistHeap.merge(%LeftistHeap{key: 1, rank: 1}, %LeftistHeap{key: 2, rank: 1})
+  %LeftistHeap{key: 1, rank: 1, left: %LeftistHeap{key: 2, rank: 1}}
+  """
   def merge(nil, nil), do: nil
   def merge(%LeftistHeap{} = heap, nil), do: heap
   def merge(nil, %LeftistHeap{} = heap), do: heap
@@ -51,26 +56,84 @@ defmodule LeftistHeap do
     end
   end
 
+  @doc """
+  Inserts an item or a list of items into a heap
+
+  ## Examples
+  iex> LeftistHeap.from_key(1) |> LeftistHeap.insert(2)
+  %LeftistHeap{key: 1, rank: 1, left: %LeftistHeap{key: 2, rank: 1}}
+  iex> LeftistHeap.from_key(1) |> LeftistHeap.insert([2, 3])
+  %LeftistHeap{
+    key: 1, rank: 1, left: %LeftistHeap{
+      key: 2, rank: 1, left: %LeftistHeap{
+        key: 3, rank: 1
+      }
+    }
+  }
+  """
   def insert(heap, list) when is_list(list), do: list |> from_list() |> merge(heap)
   def insert(heap, key), do: key |> from_key() |> merge(heap)
 
+  @doc """
+  Return the smallest element in the heap
+
+  ## Examples
+  iex> [3, 7, 2, 4, 7, 1] |> LeftistHeap.from_list() |> LeftistHeap.get_min()
+  1
+  """
   def get_min(%LeftistHeap{key: key}), do: key
   def get_min(nil), do: :empty
 
+  @doc """
+  Removes the smallest element.
+
+  ## Examples
+  iex> LeftistHeap.from_list([4, 2, 5]) |> LeftistHeap.delete_min()
+  {:ok, 2, %LeftistHeap{key: 4, rank: 1, left: %LeftistHeap{key: 5, rank: 1}}}
+  iex> LeftistHeap.delete_min(nil)
+  {:err, :empty}
+  """
   def delete_min(%LeftistHeap{right: right, left: left, key: key}),
     do: {:ok, key, merge(right, left)}
 
   def delete_min(nil), do: {:err, :empty}
 
-  def to_list(%LeftistHeap{} = heap), do: to_list([], heap)
-  def to_list(list, nil), do: Enum.reverse(list)
+  @doc """
+  Converts a heap to a list representation
 
-  def to_list(list, %LeftistHeap{} = heap) do
+  ## Examples
+  iex> LeftistHeap.from_key(2) |> LeftistHeap.insert(4) |> LeftistHeap.to_list
+  [2, 4]
+  """
+  def to_list(%LeftistHeap{} = heap), do: to_list([], heap)
+
+  # Recursive base case, when we hit a leaf we reverse the list we've built by cons
+  defp to_list(list, nil), do: Enum.reverse(list)
+
+  # Implements to_list using recursion, pops an element off the heap one by one and
+  # cons' it to the the list. Recurses until no more items are left on the heap.
+  defp to_list(list, %LeftistHeap{} = heap) do
     {:ok, min, new_heap} = delete_min(heap)
     to_list([min | list], new_heap)
   end
 
+  @doc """
+  Creates a heap from a list.
+
+  ## Examples
+  iex> LeftistHeap.from_list([1, 2, 3])
+  %LeftistHeap{
+    key: 1, rank: 1, left: %LeftistHeap{
+      key: 2, rank: 1, left: %LeftistHeap{
+        key: 3, rank: 1
+      }
+    }
+  }
+  """
   def from_list([head | list]), do: from_list(list, from_key(head))
-  def from_list([head | tail], %LeftistHeap{} = heap), do: from_list(tail, insert(heap, head))
-  def from_list([], %LeftistHeap{} = heap), do: heap
+  # Recursive implementation of from_list. Pops the first element off a list and adds it to
+  # the heap. Continues until no items remain.
+  defp from_list([head | tail], %LeftistHeap{} = heap), do: from_list(tail, insert(heap, head))
+  # Base case, returns the built up heap
+  defp from_list([], %LeftistHeap{} = heap), do: heap
 end
