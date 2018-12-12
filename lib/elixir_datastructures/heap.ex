c9f1646d1912011c2e58097e1996efd5e572cd88
John Goff <john.g@hiredhippo.ai>
2018-12-12 11:59:16 -0500

    rename heap, and work on documentation

--- lib/elixir_datastructures/heap.ex
+++ lib/elixir_datastructures/heap.ex
@@ -1,4 +1,4 @@
-defmodule Heap do
+defmodule LeftistHeap do
   @moduledoc """
   Elixir implementation of a leftist tree.
   Leaves are represented by nil. Based on the OCaml implementation available [here](http://typeocaml.com/2015/03/12/heap-leftist-tree/)
@@ -12,56 +12,65 @@ defmodule Heap do
   Will return 0 for a leaf.
 
   ## Examples
-  
+
       iex> Heap.rank(%Heap{rank: 2})
       2
 
       iex> Heap.rank(nil)
       0
   """
-  def rank(%Heap{rank: rank}), do: rank
+  def rank(%LeftistHeap{rank: rank}), do: rank
   def rank(nil), do: 0
 
-  def heap_from_key(key) when is_integer(key), do: %Heap{key: key, rank: 1}
+  @doc """
+  Creates a new heap with the given key.
+
+  ## Examples
+  iex> Heap.from_key(1)
+  %Heap{key: 1, rank: 1}
+  """
+  def from_key(key) when is_integer(key), do: %LeftistHeap{key: key, rank: 1}
 
   def merge(nil, nil), do: nil
-  def merge(%Heap{} = heap, nil), do: heap
-  def merge(nil, %Heap{} = heap), do: heap
+  def merge(%LeftistHeap{} = heap, nil), do: heap
+  def merge(nil, %LeftistHeap{} = heap), do: heap
 
-  def merge(%Heap{left: left, right: right, key: key1} = h1, %Heap{key: key2} = h2) do
+  def merge(%LeftistHeap{left: left, right: right, key: key1} = h1, %LeftistHeap{key: key2} = h2) do
     if key1 > key2 do
-      _merge(h2, h1)
+      merge(h2, h1)
     else
-      merged = _merge(right, h2)
-      rank_left = _rank(left)
-      rank_right = _rank(merged)
+      merged = merge(right, h2)
+      rank_left = rank(left)
+      rank_right = rank(merged)
 
       if rank_left >= rank_right do
-        %Heap{h1 | right: merged, rank: rank_right + 1}
+        %LeftistHeap{h1 | right: merged, rank: rank_right + 1}
       else
-        %Heap{h1 | right: left, left: merged, rank: rank_left + 1}
+        %LeftistHeap{h1 | right: left, left: merged, rank: rank_left + 1}
       end
     end
   end
 
-  def insert(heap, list) when is_list(list), do: list |> _from_list() |> _merge(heap)
-  def insert(heap, key), do: key |> _heap_from_key() |> _merge(heap)
+  def insert(heap, list) when is_list(list), do: list |> from_list() |> merge(heap)
+  def insert(heap, key), do: key |> from_key() |> merge(heap)
 
-  def get_min(%Heap{key: key}), do: key
+  def get_min(%LeftistHeap{key: key}), do: key
   def get_min(nil), do: :empty
 
-  def delete_min(%Heap{right: right, left: left, key: key}), do: {:ok, key, _merge(right, left)}
+  def delete_min(%LeftistHeap{right: right, left: left, key: key}),
+    do: {:ok, key, merge(right, left)}
+
   def delete_min(nil), do: {:err, :empty}
 
-  def to_list(%Heap{} = heap), do: _to_list([], heap)
+  def to_list(%LeftistHeap{} = heap), do: to_list([], heap)
   def to_list(list, nil), do: Enum.reverse(list)
 
-  def to_list(list, %Heap{} = heap) do
-    {:ok, min, new_heap} = _delete_min(heap)
-    _to_list([min | list], new_heap)
+  def to_list(list, %LeftistHeap{} = heap) do
+    {:ok, min, new_heap} = delete_min(heap)
+    to_list([min | list], new_heap)
   end
 
-  def from_list([head | list]), do: _from_list(list, _heap_from_key(head))
-  def from_list([head | tail], %Heap{} = heap), do: _from_list(tail, _insert(heap, head))
-  def from_list([], %Heap{} = heap), do: heap
+  def from_list([head | list]), do: from_list(list, from_key(head))
+  def from_list([head | tail], %LeftistHeap{} = heap), do: from_list(tail, insert(heap, head))
+  def from_list([], %LeftistHeap{} = heap), do: heap
 end
--- lib/elixir_datastructures/heap_server.ex
+++ lib/elixir_datastructures/heap_server.ex
@@ -1,17 +1,16 @@
-defmodule Heap do
+defmodule HeapServer do
   use GenServer
-  # Struct implementing a leftist tree. Leaves are represented by nil.
-  defstruct left: nil, key: 0, right: nil, rank: 0
+  alias LeftistHeap, as: Heap
 
   # Client 
   def start_link,
     do: GenServer.start_link(__MODULE__, %Heap{}, name: ElixirHeap)
 
   def start_link(list) when is_list(list),
-    do: GenServer.start_link(__MODULE__, _from_list(list), name: ElixirHeap)
+    do: GenServer.start_link(__MODULE__, Heap.from_list(list), name: ElixirHeap)
 
   def start_link(item),
-    do: GenServer.start_link(__MODULE__, _heap_from_key(item), name: ElixirHeap)
+    do: GenServer.start_link(__MODULE__, Heap.from_key(item), name: ElixirHeap)
 
   def min(), do: GenServer.call(ElixirHeap, :min)
 
@@ -21,7 +20,7 @@ defmodule Heap do
 
   def insert(item), do: GenServer.cast(ElixirHeap, {:insert, item})
 
-  def remove() do
+  def delete_min() do
     minimum = min()
 
     case GenServer.cast(ElixirHeap, :delete_min) do
@@ -35,69 +34,22 @@ defmodule Heap do
   def init(%Heap{}), do: {:ok, %Heap{}}
 
   @impl true
-  def handle_call(:size, _from, heap), do: {:reply, length(_to_list(heap)), heap}
+  def handle_call(:min, _from, heap), do: {:reply, Heap.get_min(heap), heap}
 
   @impl true
-  def handle_call(:min, _from, heap), do: {:reply, _get_min(heap), heap}
+  def handle_call(:size, _from, heap), do: {:reply, length(Heap.to_list(heap)), heap}
 
   @impl true
-  def handle_call(:to_list, _from, heap), do: {:reply, _to_list(heap), heap}
+  def handle_call(:to_list, _from, heap), do: {:reply, Heap.to_list(heap), heap}
 
   @impl true
-  def handle_cast({:insert, item}, heap), do: {:noreply, _insert(heap, item)}
+  def handle_cast({:insert, item}, heap), do: {:noreply, Heap.insert(heap, item)}
 
   @impl true
   def handle_cast(:delete_min, heap) do
-    case _delete_min(heap) do
+    case Heap.delete_min(heap) do
       {:ok, _removed, new_heap} -> {:noreply, new_heap}
       _ -> {:noreply, heap}
     end
   end
-
-  # Implementation of leftist heap data structure, loosely based on: http://typeocaml.com/2015/03/12/heap-leftist-tree/
-  def _rank(%Heap{rank: rank}), do: rank
-  def _rank(nil), do: 0
-
-  def _heap_from_key(key) when is_integer(key), do: %Heap{key: key, rank: 1}
-
-  def _merge(nil, nil), do: nil
-  def _merge(%Heap{} = heap, nil), do: heap
-  def _merge(nil, %Heap{} = heap), do: heap
-
-  def _merge(%Heap{left: left, right: right, key: key1} = h1, %Heap{key: key2} = h2) do
-    if key1 > key2 do
-      _merge(h2, h1)
-    else
-      merged = _merge(right, h2)
-      rank_left = _rank(left)
-      rank_right = _rank(merged)
-
-      if rank_left >= rank_right do
-        %Heap{h1 | right: merged, rank: rank_right + 1}
-      else
-        %Heap{h1 | right: left, left: merged, rank: rank_left + 1}
-      end
-    end
-  end
-
-  def _insert(heap, list) when is_list(list), do: list |> _from_list() |> _merge(heap)
-  def _insert(heap, key), do: key |> _heap_from_key() |> _merge(heap)
-
-  def _get_min(%Heap{key: key}), do: key
-  def _get_min(nil), do: :empty
-
-  def _delete_min(%Heap{right: right, left: left, key: key}), do: {:ok, key, _merge(right, left)}
-  def _delete_min(nil), do: {:err, :empty}
-
-  def _to_list(%Heap{} = heap), do: _to_list([], heap)
-  def _to_list(list, nil), do: Enum.reverse(list)
-
-  def _to_list(list, %Heap{} = heap) do
-    {:ok, min, new_heap} = _delete_min(heap)
-    _to_list([min | list], new_heap)
-  end
-
-  def _from_list([head | list]), do: _from_list(list, _heap_from_key(head))
-  def _from_list([head | tail], %Heap{} = heap), do: _from_list(tail, _insert(heap, head))
-  def _from_list([], %Heap{} = heap), do: heap
 end
