5b887dd3d7b5365970d82c96720a92f0a451b169
John Goff <john.g@hiredhippo.ai>
2018-12-12 20:12:32 -0500

    continue with lesson plan and rearrange heap.ex

--- lesson-plan.md
+++ lesson-plan.md
@@ -9,8 +9,36 @@ stack.ex examine code
 * linked lists
 iex> import_file "stack.ex"
 iex> {:ok, pid} = GenServer.start_link(Stack, [])
-iex> pid |> GenServer.cast({:push, :hippo})
-iex> pid |> GenServer.cast({:push, :hired})
-iex> pid |> GenServer.call()
-iex> pid |> GenServer.call()
-iex> pid |> GenServer.call()
+iex> pid |> GenServer.cast({:push, :world})
+iex> pid |> GenServer.cast({:push, :hello})
+iex> pid |> GenServer.call(:pop)
+iex> pid |> GenServer.call(:pop)
+iex> pid |> GenServer.call(:pop)
+* It crashed.
+  - doesn't respond to any further messages
+* That's ok, we can restart it.
+* But what if we don't want to restart it every time?
+* Use a Supervisor
+* need client functions for that
+stack2.ex examine code
+* same as last time, but we have an api instead of calling GenServer manually
+* we can use this api, so can Supervisor
+iex> import_file "stack2.ex"
+iex> StackTwo.start_link([:world])
+iex> StackTwo.push(:hello)
+iex> StackTwo.pop()
+iex> StackTwo.pop()
+iex> StackTwo.pop()
+* Same crash as before, just nicer to read and type
+iex> {:ok, pid} = Supervisor.start_link([{StackTwo, [:world]}, strategy: :one_for_one)
+iex> StackTwo.push(:hello)
+iex> StackTwo.pop()
+iex> StackTwo.pop()
+iex> StackTwo.pop()
+* Same crash, but this time, if we try and continue to use it, it works!
+iex> StackTwo.push(:world)
+iex> StackTwo.push(:hello)
+iex> StackTwo.pop()
+iex> StackTwo.pop()
+heap.ex examine code
+* Ok, time for a more serious data structre
--- lib/elixir_datastructures/heap.ex
+++ lib/elixir_datastructures/heap.ex
@@ -6,23 +6,6 @@ defmodule LeftistHeap do
   defstruct left: nil, key: 0, right: nil, rank: 0
 
   @doc """
-  Gets the rank of a heap.
-
-  Returns the number representing the distance between the node and the rightmost leaf.
-  Will return 0 for a leaf.
-
-  ## Examples
-  ```
-  iex> LeftistHeap.rank(%LeftistHeap{rank: 2})
-  2
-  iex> LeftistHeap.rank(nil)
-  0
-  ```
-  """
-  def rank(%LeftistHeap{rank: rank}), do: rank
-  def rank(nil), do: 0
-
-  @doc """
   Creates a new heap with the given key.
 
   ## Examples
@@ -34,62 +17,20 @@ defmodule LeftistHeap do
   def from_key(key) when is_integer(key), do: %LeftistHeap{key: key, rank: 1}
 
   @doc """
-  Returns the size of the heap
-
-  ## Dxamples
-  iex> [1, 2, 3] |> LeftistHeap.from_list() |> LeftistHeap.size()
-  3
-  """
-  def size(heap), do: heap |> to_list() |> length()
-
-  @doc """
-  Merges two heaps together
+  Creates a heap from a list.
 
   ## Examples
   ```
-  iex> LeftistHeap.merge(%LeftistHeap{key: 1, rank: 1}, %LeftistHeap{key: 2, rank: 1})
-  %LeftistHeap{key: 1, rank: 1, left: %LeftistHeap{key: 2, rank: 1}}
+  iex> LeftistHeap.from_list([1, 2, 3])
+  %LeftistHeap{key: 1, rank: 2, left: %LeftistHeap{key: 2, rank: 1}, right: %LeftistHeap{key: 3, rank: 1}}
   ```
   """
-  def merge(nil, nil), do: nil
-  def merge(%LeftistHeap{} = heap, nil), do: heap
-  def merge(nil, %LeftistHeap{} = heap), do: heap
-
-  def merge(%LeftistHeap{left: left, right: right, key: key1} = h1, %LeftistHeap{key: key2} = h2) do
-    if key1 > key2 do
-      merge(h2, h1)
-    else
-      merged = merge(right, h2)
-      rank_left = rank(left)
-      rank_right = rank(merged)
-
-      if rank_left >= rank_right do
-        %LeftistHeap{h1 | right: merged, rank: rank_right + 1}
-      else
-        %LeftistHeap{h1 | right: left, left: merged, rank: rank_left + 1}
-      end
-    end
-  end
-
-  @doc """
-  Inserts an item or a list of items into a heap
-
-  ## Examples
-  ```
-  iex> LeftistHeap.from_key(1) |> LeftistHeap.insert(2)
-  %LeftistHeap{key: 1, rank: 1, left: %LeftistHeap{key: 2, rank: 1}}
-  iex> LeftistHeap.from_key(1) |> LeftistHeap.insert([2, 3])
-  %LeftistHeap{
-    key: 1, rank: 1, left: %LeftistHeap{
-      key: 2, rank: 1, left: %LeftistHeap{
-        key: 3, rank: 1
-      }
-    }
-  }
-  ```
-  """
-  def insert(heap, list) when is_list(list), do: list |> from_list() |> merge(heap)
-  def insert(heap, key), do: key |> from_key() |> merge(heap)
+  def from_list([head | list]), do: from_list(list, from_key(head))
+  # Recursive implementation of from_list. Pops the first element off a list and adds it to
+  # the heap. Continues until no items remain.
+  defp from_list([head | tail], %LeftistHeap{} = heap), do: from_list(tail, insert(heap, head))
+  # Base case, returns the built up heap
+  defp from_list([], %LeftistHeap{} = heap), do: heap
 
   @doc """
   Return the smallest element in the heap
@@ -102,20 +43,13 @@ defmodule LeftistHeap do
   def get_min(nil), do: :empty
 
   @doc """
-  Removes the smallest element.
+  Returns the size of the heap
 
-  ## Examples
-  ```
-  iex> LeftistHeap.from_list([4, 2, 5]) |> LeftistHeap.delete_min()
-  {:ok, 2, %LeftistHeap{key: 4, rank: 1, left: %LeftistHeap{key: 5, rank: 1}}}
-  iex> LeftistHeap.delete_min(nil)
-  {:err, :empty}
-  ```
+  ## Dxamples
+  iex> [1, 2, 3] |> LeftistHeap.from_list() |> LeftistHeap.size()
+  3
   """
-  def delete_min(%LeftistHeap{right: right, left: left, key: key}),
-    do: {:ok, key, merge(right, left)}
-
-  def delete_min(nil), do: {:err, :empty}
+  def size(heap), do: heap |> to_list() |> length()
 
   @doc """
   Converts a heap to a list representation
@@ -139,18 +73,64 @@ defmodule LeftistHeap do
   end
 
   @doc """
-  Creates a heap from a list.
+  Inserts an item or a list of items into a heap
 
   ## Examples
   ```
-  iex> LeftistHeap.from_list([1, 2, 3])
-  %LeftistHeap{key: 1, rank: 2, left: %LeftistHeap{key: 2, rank: 1}, right: %LeftistHeap{key: 3, rank: 1}}
+  iex> LeftistHeap.from_key(1) |> LeftistHeap.insert(2)
+  %LeftistHeap{key: 1, rank: 1, left: %LeftistHeap{key: 2, rank: 1}}
+  iex> LeftistHeap.from_key(1) |> LeftistHeap.insert([2, 3])
+  %LeftistHeap{
+    key: 1, rank: 1, left: %LeftistHeap{
+      key: 2, rank: 1, left: %LeftistHeap{
+        key: 3, rank: 1
+      }
+    }
+  }
   ```
   """
-  def from_list([head | list]), do: from_list(list, from_key(head))
-  # Recursive implementation of from_list. Pops the first element off a list and adds it to
-  # the heap. Continues until no items remain.
-  defp from_list([head | tail], %LeftistHeap{} = heap), do: from_list(tail, insert(heap, head))
-  # Base case, returns the built up heap
-  defp from_list([], %LeftistHeap{} = heap), do: heap
+  def insert(heap, list) when is_list(list), do: list |> from_list() |> merge(heap)
+  def insert(heap, key), do: key |> from_key() |> merge(heap)
+
+  @doc """
+  Removes the smallest element.
+
+  ## Examples
+  ```
+  iex> LeftistHeap.from_list([4, 2, 5]) |> LeftistHeap.delete_min()
+  {:ok, 2, %LeftistHeap{key: 4, rank: 1, left: %LeftistHeap{key: 5, rank: 1}}}
+  iex> LeftistHeap.delete_min(nil)
+  {:err, :empty}
+  ```
+  """
+  def delete_min(%LeftistHeap{right: right, left: left, key: key}),
+    do: {:ok, key, merge(right, left)}
+
+  def delete_min(nil), do: {:err, :empty}
+
+  # Merges two heaps together
+  defp merge(nil, nil), do: nil
+  defp merge(%LeftistHeap{} = heap, nil), do: heap
+  defp merge(nil, %LeftistHeap{} = heap), do: heap
+
+  defp merge(%LeftistHeap{left: left, right: right, key: key1} = h1, %LeftistHeap{key: key2} = h2) do
+    if key1 > key2 do
+      merge(h2, h1)
+    else
+      merged = merge(right, h2)
+      rank_left = rank(left)
+      rank_right = rank(merged)
+
+      if rank_left >= rank_right do
+        %LeftistHeap{h1 | right: merged, rank: rank_right + 1}
+      else
+        %LeftistHeap{h1 | right: left, left: merged, rank: rank_left + 1}
+      end
+    end
+  end
+
+  # Returns the number representing the distance between the node and the rightmost leaf.
+  # Will return 0 for a leaf.
+  defp rank(%LeftistHeap{rank: rank}), do: rank
+  defp rank(nil), do: 0
 end
